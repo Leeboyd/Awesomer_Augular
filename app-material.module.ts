3e751b050d7c4c8c512a9fcd36a4b0d232a459eb
LeeBoy <jobboy19890101@gmail.com>
2018-01-30 11:10:04 +0800

    Material 引用 ＆ template binding

---
+++ app-material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { 
+  MatButtonModule,
+  MatInputModule
+} from '@angular/material';
+
+@NgModule({
+  // imports: [MatButtonModule],
+  exports: [
+    MatButtonModule,
+    MatInputModule
+  ]
+})
+export class AppMaterialModule {
+}
--- app.module.ts
+++ app.module.ts
@@ -1,7 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser'; 
-import { NgModule } from '@angular/core'; 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
  
 import { AppRoutingModule } from './app-routing.module'
+import { AppMaterialModule } from './app-material.module';
+
 import { AppComponent } from './app.component';
 import { WidgetsComponent } from './widgets/widgets.component'; 
 
@@ -18,7 +22,10 @@ import { WidgetsComponent } from './widgets/widgets.component';
   ], 
   imports: [ 
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    AppMaterialModule
   ], 
   providers: [], 
   bootstrap: [AppComponent] 
