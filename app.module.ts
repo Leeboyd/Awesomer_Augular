f604b704b913125988a99c60705ee9f37141fd63
LeeBoy <jobboy19890101@gmail.com>
2018-01-31 10:40:04 +0800

    sync 01 and

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,12 @@
+// ng API
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
- 
 import { AppRoutingModule } from './app-routing.module'
 import { AppMaterialModule } from './app-material.module';
 
+// My Component
 import { AppComponent } from './app.component';
 import { WidgetsComponent } from './widgets/widgets.component'; 
 
@@ -14,6 +15,7 @@ import { WidgetsComponent } from './widgets/widgets.component';
 
  
 // Service 
+import { WidgetsService } from './Services/services';
 
 @NgModule({ 
   declarations: [ 
@@ -27,7 +29,9 @@ import { WidgetsComponent } from './widgets/widgets.component';
     NoopAnimationsModule,
     AppMaterialModule
   ], 
-  providers: [], 
+  providers: [
+    WidgetsService
+  ], 
   bootstrap: [AppComponent] 
 }) 
 export class AppModule { } 
--- widgets/widgets.component.ts
+++ widgets/widgets.component.ts
@@ -1,55 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WidgetsService } from '../Services/widgets.services'
 @Component({
   selector: 'app-widgets',
   templateUrl: './widgets.component.html',
   styleUrls: ['./widgets.component.css']
 })
 export class WidgetsComponent implements OnInit {
-  widgets = [
-    {
-      id: 1,
-      name: 'Red Widget',
-      description: 'This is a red widget'
-    },
-    {
-      id: 2,
-      name: 'Orange Widget',
-      description: 'This is an orange widget'
-    },
-    {
-      id: 3,
-      name: 'Yellow Widget',
-      description: 'This is a yellow widget'
-    },
-    {
-      id: 4,
-      name: 'Green Widget',
-      description: 'This is a green widget'
-    },
-    {
-      id: 5,
-      name: 'Blue Widget',
-      description: 'This is a blue widget'
-    },
-    {
-      id: 6,
-      name: 'Indigo Widget',
-      description: 'This is a indigo widget'
-    },
-    {
-      id: 7,
-      name: 'Violet Widget',
-      description: 'This is a violet widget'
-    }
-  ]
+  selectedWidget;
+  widgets;
 
-  constructor() { }
+  constructor(private widgetsService: WidgetsService) { }
 
   ngOnInit() {
+    this.widgets = this.widgetsService.widgets
   }
 
-  echo(message) {
-    console.log(`COLOR: ${message}`)
+  selected (widget) {
+    this.selectedWidget = widget
   }
 }
