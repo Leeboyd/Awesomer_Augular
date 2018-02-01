667b18b59a44bd0584b67e194ad8d7861d03dab6
LeeBoy <jobboy19890101@gmail.com>
2018-02-01 09:21:31 +0800

    review Day1.5

--- Components/widgets/widgets.component.ts
+++ Components/widgets/widgets.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { WidgetsService } from '@app/Services/widgets.services'
+import { Widget } from './widgets.interface'
 @Component({
   selector: 'app-widgets',
   templateUrl: './widgets.component.html',
   styleUrls: ['./widgets.component.css']
 })
 export class WidgetsComponent implements OnInit {
-  selectedWidget;
-  widgets;
+  selectedWidget: Widget;
+  widgets: Widget[];
 
   constructor(private widgetsService: WidgetsService) { }
 
   ngOnInit() {
     this.widgets = this.widgetsService.widgets
+    this.reset()
+  }
+
+  reset() {
+    this.selectedWidget = { id: null, name: '', description: ''}
   }
 
   selected (widget) {
     this.selectedWidget = widget
+  }
+
+  save (widget) {
+    console.log(`Successfully update: ${JSON.stringify(widget)}`) 
+    this.reset() 
   }
 }
